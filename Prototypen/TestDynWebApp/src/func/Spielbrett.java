3bce5cfe0c2ea1da541fe29adb2d81a3713f5681
WolfMiri <Miriam.Wolf@heidelberg.com>
2017-10-07 15:50:41 +0200

    Spielbrett hinzugefuegt

---
+++ Prototypen/TestDynWebApp/src/func/Spielbrett.java
@@ -0,0 +1,20 @@
+package func;
+
+/*
+ * Hier findet die ganze Spielsteuerung von Unternehmen und Markt statt
+ */
+public class Spielbrett {
+
+	// Spieler
+	
+	// Runden
+	
+	// Markt
+	
+	// -> Runde beenden: Rückgabe aktueller Spieler, Spielerdaten direkt erfassen
+	// -> Periode beenden: Marktsimmulation starten; Rückgabe der Infos an die Spieler
+	
+	// Spieler ist dran: Spielobjekt laden (Wie in HTML anzeigen?)
+	
+	
+}
