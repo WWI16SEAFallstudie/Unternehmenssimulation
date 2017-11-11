37275820ad8f11f7f8ae6562ec1db284dd694eee
Enticklerzwergin <luisa.karl@gmx.de>
2017-11-11 12:15:07 +0100

    Markttest erweitert

--- Prototypen/TestDynWebApp/src/tests/Markttest.java
+++ Prototypen/TestDynWebApp/src/tests/Markttest.java
@@ -1,12 +1,12 @@
 package tests;
 
-import func.;
+import func.*;
 import static org.junit.Assert.*; 
 import org.junit.Test;
 
 public class Markttest {
 	@Test
-	public void uhrenverkaufTest {
+	public void uhrenverkaufAlleKategorienTest() {
 		Spielbrett spielbrett = new Spielbrett(10, 10000, 0.1);
 		Unternehmen[] spieler;
 		spielbrett.erstelleSpieler(2);
@@ -32,5 +32,29 @@ public class Markttest {
 		assertEquals("Uhren wurde nicht korrekt abgenommen", 50, uhren[0].getAbgenommeneMenge());
 		assertEquals("Uhren wurde nicht korrekt abgenommen", 50, uhren[1].getAbgenommeneMenge());
 		assertEquals("Uhren wurde nicht korrekt abgenommen", 50, uhren[2].getAbgenommeneMenge());
+	}
+	
+	@Test
+	public void uhrenverkaufInGleicherKategorieTest() {
+		Spielbrett spielbrett = new Spielbrett(10, 10000, 0.1);
+		Unternehmen[] spieler;
+		spielbrett.erstelleSpieler(2);
+		spieler = spielbrett.getSpieler();
+		spieler[0].freischaltenSegment("Billig");
+		spieler[0].freischaltenSegment("Billig");
+		spieler[0].erforscheUhr("Billig");
+		spieler[0].erforscheUhr("Billig");	
+		spieler[0].produzieren(100, 0);
+		spieler[0].produzieren(100, 1);
+		iUhrenkategorie[] uhren = spieler[0].getUhr();
+		uhren[0].setMarktwert(100);
+		uhren[1].setMarktwert(150);
+		spieler[0].bieteUhren(100, 0, 100);
+		spieler[0].bieteUhren(100, 1, 100);
+		spielbrett.starteMarkt(true);
+		System.out.println(uhren[0].getAbgenommeneMenge());
+
+//		assertEquals("Uhren wurde nicht korrekt abgenommen", 50, uhren[0].getAbgenommeneMenge());
+//		assertEquals("Uhren wurde nicht korrekt abgenommen", 50, uhren[1].getAbgenommeneMenge());
 	}
 }
