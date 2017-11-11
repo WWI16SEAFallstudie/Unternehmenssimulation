6729583273731f2325ca21ac974b1a4fdb28cd86
Tilman <Tilman.Hess@gmx.de>
2017-11-11 23:31:42 +0100

    Markttest fix
    Korrektur von Kommunikationsproblemen :P

--- Prototypen/TestDynWebApp/src/tests/Markttest.java
+++ Prototypen/TestDynWebApp/src/tests/Markttest.java
@@ -24,9 +24,9 @@ public class Markttest {
 		uhren[0].setMarktwert(100);
 		uhren[1].setMarktwert(100);
 		uhren[2].setMarktwert(100);
-		spieler[0].setAngeboteneMenge(100, 0, 100);
-		spieler[0].setAngeboteneMenge(100, 1, 100);
-		spieler[0].setAngeboteneMenge(100, 2, 100);
+		spieler[0].bieteUhren(100, 0, 100);
+		spieler[0].bieteUhren(100, 1, 100);
+		spieler[0].bieteUhren(100, 2, 100);
 		spielbrett.starteMarkt(true);
 
 		assertEquals("Uhren wurde nicht korrekt abgenommen", 50, uhren[0].getAbgenommeneMenge());
@@ -41,7 +41,6 @@ public class Markttest {
 		spielbrett.erstelleSpieler(2);
 		spieler = spielbrett.getSpieler();
 		spieler[0].freischaltenSegment("Billig");
-		spieler[0].freischaltenSegment("Billig");
 		spieler[0].erforscheUhr("Billig");
 		spieler[0].erforscheUhr("Billig");	
 		spieler[0].produzieren(100, 0);
@@ -49,11 +48,29 @@ public class Markttest {
 		iUhrenkategorie[] uhren = spieler[0].getUhr();
 		uhren[0].setMarktwert(100);
 		uhren[1].setMarktwert(150);
-		spieler[0].setAngeboteneMenge(100, 0, 100);
-		spieler[0].setAngeboteneMenge(100, 1, 100);
+		spieler[0].bieteUhren(100, 0, 100);
+		spieler[0].bieteUhren(100, 1, 100);
 		spielbrett.starteMarkt(true);
 
 		assertEquals("Uhren wurde nicht korrekt abgenommen", 41, uhren[0].getAbgenommeneMenge());
 		assertEquals("Uhren wurde nicht korrekt abgenommen", 66, uhren[1].getAbgenommeneMenge());
+	}
+	
+	@Test
+	public void uhrenverkaufOhneLagerbestand() {
+		Spielbrett spielbrett = new Spielbrett(10, 10000, 0.1);
+		Unternehmen[] spieler;
+		spielbrett.erstelleSpieler(2);
+		spieler = spielbrett.getSpieler();
+		spieler[0].freischaltenSegment("Billig");
+		spieler[0].erforscheUhr("Billig");
+		spieler[0].produzieren(10, 0);
+		iUhrenkategorie[] uhren = spieler[0].getUhr();
+		uhren[0].setAngebotspreis(100);
+		uhren[0].setAngeboteneMenge(100);
+		uhren[0].setMarktwert(100);
+		spielbrett.starteMarkt(true);
+		
+		assertEquals("Uhren wurden verkauft ohne Bestand", 5, uhren[0].getAbgenommeneMenge());
 	}
 }
